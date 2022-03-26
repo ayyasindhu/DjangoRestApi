4614c9f0aa5f220fb4f852841f424d7e76a38b02
Sindhu <ayyasindhu9@gmail.com>
2022-03-26 23:10:12 +0100

    test

--- bookInventorys/views.py
+++ bookInventorys/views.py
@@ -8,9 +8,13 @@ from bookInventorys.models import BookInventory
 from bookInventorys.serializers import BookInventorySerializer
 from rest_framework.decorators import api_view
 
+#Creating the BookInventory Array and storing the books details inthat perticular Array
 
 @api_view(['GET', 'POST', 'DELETE'])
 def bookInventory_list(request):
+
+# Getting all list of book names into the Array
+
     if request.method == 'GET':
         bookInventorys = BookInventory.objects.all()
         
@@ -21,7 +25,9 @@ def bookInventory_list(request):
         bookInventorys_serializer = BookInventorySerializer(bookInventorys, many=True)
         return JsonResponse(bookInventorys_serializer.data, safe=False)
         # 'safe=False' for objects serialization
- 
+
+  #Adding the new books to the List of Books
+
     elif request.method == 'POST':
         bookInventory_data = JSONParser().parse(request)
         bookInventory_serializer = BookInventorySerializer(data=bookInventory_data)
@@ -29,23 +35,30 @@ def bookInventory_list(request):
             bookInventory_serializer.save()
             return JsonResponse(bookInventory_serializer.data, status=status.HTTP_201_CREATED) 
         return JsonResponse(bookInventory_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+   # Removing the all list of books from the BookInventory    
+  
     elif request.method == 'DELETE':
         count = BookInventory.objects.all().delete()
         return JsonResponse({'message': '{} BookInventorys were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
  
- 
+  #To display the requested book by using ID as 'pk' from the Book Inventory 
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def bookInventory_detail(request, pk):
     try: 
         bookInventory = BookInventory.objects.get(pk=pk) 
     except BookInventory.DoesNotExist: 
         return JsonResponse({'message': 'The bookInventory does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+
+ #Getting the requred book from the BookInventory
  
     if request.method == 'GET': 
         bookInventory_serializer = BookInventorySerializer(bookInventory) 
         return JsonResponse(bookInventory_serializer.data) 
- 
+
+  #Displaying the requested book from the BookInventory
+
     elif request.method == 'PUT': 
         bookInventory_data = JSONParser().parse(request) 
         bookInventory_serializer = BookInventorySerializer(bookInventory, data=bookInventory_data) 
@@ -53,10 +66,14 @@ def bookInventory_detail(request, pk):
             bookInventory_serializer.save() 
             return JsonResponse(bookInventory_serializer.data) 
         return JsonResponse(bookInventory_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
- 
+
+  #Deleting the requested book from the BookInventory
+
     elif request.method == 'DELETE': 
         bookInventory.delete() 
         return JsonResponse({'message': 'BookInventory was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+ #Displaying the requested book from the BookInventory
     
         
 @api_view(['GET'])
